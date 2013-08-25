b13dbf6d532d03e904481a055c9096cc2fe2d4bf
Yu Takasuka <yu@takasuka.net>
2013-08-26 00:37:13 +0800

    Add LogMessage and Override Lifecycle methods

--- GeoQuiz/src/net/takasuka/GeoQuiz/QuizActivity.java
+++ GeoQuiz/src/net/takasuka/GeoQuiz/QuizActivity.java
@@ -2,6 +2,7 @@ package net.takasuka.GeoQuiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +33,7 @@ public class QuizActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.main);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -61,6 +63,36 @@ public class QuizActivity extends Activity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
