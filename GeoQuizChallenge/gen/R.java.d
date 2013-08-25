b9d9d7d6a42bd4074d8f98faae08601796bd9886
Yu Takasuka <yu@takasuka.net>
2013-08-25 23:48:27 +0800

    init commit

---
+++ GeoQuizChallenge/gen/R.java.d
@@ -0,0 +1,8 @@
+/Users/takasuka-y/pj/GeoQuiz/gen/net/takasuka/GeoQuiz/R.java \
+ : /Users/takasuka-y/pj/GeoQuiz/res/drawable-hdpi/ic_launcher.png \
+/Users/takasuka-y/pj/GeoQuiz/res/drawable-ldpi/ic_launcher.png \
+/Users/takasuka-y/pj/GeoQuiz/res/drawable-mdpi/ic_launcher.png \
+/Users/takasuka-y/pj/GeoQuiz/res/drawable-xhdpi/ic_launcher.png \
+/Users/takasuka-y/pj/GeoQuiz/res/layout/main.xml \
+/Users/takasuka-y/pj/GeoQuiz/res/values/strings.xml \
+/Users/takasuka-y/pj/GeoQuiz/bin/AndroidManifest.xml \
---
+++ GeoQuizChallenge/src/net/takasuka/GeoQuiz/QuizActivity.java
@@ -0,0 +1,81 @@
+package net.takasuka.GeoQuiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+}
