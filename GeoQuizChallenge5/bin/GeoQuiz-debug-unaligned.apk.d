0175eeb96fbb1c8c4914e20bf58f27faaaa4bcc0
Yu Takasuka <github@takasuka.net>
2013-08-26 15:45:04 +0800

    create new module

---
+++ GeoQuizChallenge5/bin/GeoQuiz-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/Users/takasuka-y/pj/GeoQuiz/bin/GeoQuiz-debug-unaligned.apk : \
+/Users/takasuka-y/pj/GeoQuiz/bin/GeoQuiz.ap_ \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes.dex \
---
+++ GeoQuizChallenge5/bin/classes.dex.d
@@ -0,0 +1,12 @@
+/Users/takasuka-y/pj/GeoQuiz/bin/classes.dex : \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/BuildConfig.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/Manifest.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/QuizActivity$1.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/QuizActivity$2.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/QuizActivity.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R$attr.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R$drawable.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R$id.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R$layout.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R$string.class \
+/Users/takasuka-y/pj/GeoQuiz/bin/classes/net/takasuka/GeoQuiz/R.class \
