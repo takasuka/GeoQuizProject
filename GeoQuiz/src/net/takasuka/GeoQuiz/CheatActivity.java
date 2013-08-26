1ec4ce2ee7e0cc50ab7a1b4e2ed9ecc3756ac218
takasuka <yu@takasuka.net>
2013-08-26 14:08:40 +0800

    Create new Activity Sub Class

---
+++ GeoQuiz/src/net/takasuka/GeoQuiz/CheatActivity.java
@@ -0,0 +1,11 @@
+package net.takasuka.GeoQuiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
